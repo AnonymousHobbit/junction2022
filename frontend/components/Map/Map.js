0ec99bfec2098b9e7eada7364038ab1cf2da74bf
Horjamo Olivia V <hdolivia@cubbli22-gold.local>
2022-11-05 20:21:01 +0200

    allow selecting dropoff location

--- frontend/components/Map/Map.js
+++ frontend/components/Map/Map.js
@@ -56,7 +56,7 @@ const Map = ({ coordinates, locations }) => {
             }}>
                 <Popup position={locations[key]}>
                     Drop-off alternative: <pre>{key.split(',')[0]}, price: {price[0]} <br></br> esimated duration of shipment: {price[1]}</pre>
-                    <button onClick = {e => selectDropoff(key)} >select this drop-off location</button>
+                    <button onClick = {e => selectDropoff(locations[key])} >select this drop-off location</button>
                 </Popup>
             </Marker>
             ))}
